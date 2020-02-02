7c26f16949b35b512eece3614d4d0d6b930ba6f2
Jeong Gwanjin <jeong.gwanjin@revamp.co.jp>
2020-02-02 15:34:59 +0900

    change design

--- app/src/main/java/com/information/japanesetranslation/MainActivity.java
+++ app/src/main/java/com/information/japanesetranslation/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         mContext = this;
         final EditText sentence = findViewById(R.id.sentence);
         mTextView = findViewById(R.id.result);
+        final InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 RequestHttpURLConnection conn = new RequestHttpURLConnection(mTextView);
                 conn.execute(sentence.getText().toString());
+                imm.hideSoftInputFromWindow(sentence.getWindowToken(), 0);
             }
         });
 
